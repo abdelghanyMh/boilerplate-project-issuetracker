e87900f9b3e3b7edb6a9bb4c88956aca5e02090d
abdelghanyMh <wboductqr@relay.firefox.com>
2021-07-29 17:28:15 +0100

    'adding main functionalities'

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,123 @@
 'use strict';
 
-module.exports = function (app) {
+const ObjectID = require('mongodb').ObjectID;
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      let project = req.params.project;
-      
+
+
+module.exports = function(app, myDataBase) {
+
+    app.route('/api/issues/:project')
+
+    .get(function(req, res) {
+        let project = req.params.project;
+
+        const {
+            open,
+            issue_title,
+            issue_text,
+            created_by,
+            assigned_to,
+            status_text,
+        } = req.query;
+
+        myDataBase.aggregate(
+                [
+                    { $match: { project: project } },
+                    { $unwind: '$issues' },
+                    open != '' ? { $match: { "issues.open": JSON.parse(open) } } : { $match: {} },
+                    issue_title != '' ? { $match: { "issues.issue_title": issue_title } } : { $match: {} },
+                    issue_text != '' ? { $match: { "issues.issue_text": issue_text } } : { $match: {} },
+                    created_by != '' ? { $match: { "issues.created_by": created_by } } : { $match: {} },
+                    assigned_to != '' ? { $match: { "issues.assigned_to": assigned_to } } : { $match: {} },
+                    status_text != '' ? { $match: { "issues.status_text": status_text } } : { $match: {} }
+                ]
+            ).toArray()
+            .then(respond => {
+                let mappedData = respond.map((item) => item.issues);
+                return res == null ? res.json([]) : res.json(mappedData);
+            })
+
     })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      
+
+    .post(function(req, res) {
+        let project = req.params.project;
+        // handle required field(s) missing
+        if (!req.body.issue_title || !req.body.issue_text || !req.body.created_by)
+            return res.json({ error: 'required field(s) missing' });
+        const newIssue = {
+            _id: new ObjectID(),
+            issue_title: req.body.issue_title,
+            issue_text: req.body.issue_text,
+            created_by: req.body.created_by,
+            assigned_to: req.body.assigned_to,
+            status_text: req.body.status_text,
+            created_on: new Date(),
+            updated_on: new Date(),
+            open: true
+        };
+        myDataBase.findOneAndUpdate({ project: project }, {
+                $setOnInsert: { // handel new project has issue (must add upsert: true in options)
+                    project: project
+                },
+                $push: { issues: newIssue }
+            }, { upsert: true, new: true },
+            (err, doc) => {
+                return res.json(doc);
+            }
+
+        );
     })
-    
-    .put(function (req, res){
-      let project = req.params.project;
-      
+
+    .put(function(req, res) {
+        let project = req.params.project;
+        let open = JSON.parse(req.body.open);
+        let id = new ObjectID(req.body._id);
+
+        //handel  PUT request  does not include an _id
+        if (!id)
+            return res.json({ error: 'missing _id' });
+
+        // handel PUT request  does not include update fields
+        if (!id || !open)
+            return res.json({ error: 'no update field(s) sent', '_id': id });
+
+        myDataBase.updateOne({ 'project': project, 'issues._id': id }, { $set: { 'issues.$.open': open, 'issues.$.updated_on': new Date() } }, { new: true },
+            (err, result) => {
+                console.log(`${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`);
+
+                if (result.matchedCount < 1 || err)
+                    return res.json({ error: 'could not update', '_id': id });
+                if (result.modifiedCount == 1)
+                    return res.json({ result: 'successfully updated', '_id': id });
+            }
+        )
     })
-    
-    .delete(function (req, res){
-      let project = req.params.project;
-      
+
+    .delete(function(req, res) {
+        let project = req.params.project;
+        let id = new ObjectID(req.body._id);
+
+        //handel  PUT request  does not include an _id
+        if (!id)
+            return res.json({ error: 'missing _id' });
+
+        myDataBase.findOneAndUpdate({ 'project': project }, { $pull: { issues: { _id: id } } }, { new: true }, (err, result) => {
+            if (err) {
+                return res.json({ error: 'could not delete', '_id': id });
+            }
+            return res.json({ result: 'successfully deleted', '_id': id });
+        })
+
+
+
     });
-    
+
+    //404 Not Found Middleware
+    app.use(function(req, res, next) {
+        res.status(404)
+            .type('text')
+            .send('Not Found');
+    });
+
+
 };
--- server.js
+++ server.js
@@ -1,66 +1,77 @@
 'use strict';
-
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const expect      = require('chai').expect;
-const cors        = require('cors');
 require('dotenv').config();
+const express = require('express');
+const myDB = require('./db-connection');
+const bodyParser = require('body-parser');
+const expect = require('chai').expect;
+const cors = require('cors');
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
 
 let app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
 
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Sample front-end
 app.route('/:project/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/issue.html');
-  });
+    .get(function(req, res) {
+        res.sendFile(process.cwd() + '/views/issue.html');
+    });
 
 //Index page (static HTML)
 app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+    .get(function(req, res) {
+        res.sendFile(process.cwd() + '/views/index.html');
+    });
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+myDB(async(client) => {
+    const myDataBase = await client.db('FFC_ISSUE_TRACKER').collection('projects');
+
+    //For FCC testing purposes
+    fccTestingRoutes(app, myDataBase);
+
+
+    //Routing for API 
+    apiRoutes(app, myDataBase);
+
+}).catch((e) => {
+    app.route('/').get((req, res) => {
+        res.send(`${e}, message: 'Unable to login' `);
+    });
 });
 
+
+
+
+
+
+
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-        let error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
-      }
-    }, 3500);
-  }
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Listening on port " + process.env.PORT);
+    if (process.env.NODE_ENV === 'test') {
+        console.log('Running Tests...');
+        setTimeout(function() {
+            try {
+                runner.run();
+            } catch (e) {
+                let error = e;
+                console.log('Tests are not valid:');
+                console.log(error);
+            }
+        }, 3500);
+    }
 });
 
 module.exports = app; //for testing
